ad568843e6f3373e98963a6fb33531e5e6456e0b
Aleksei Kirilenko <aleksei@mbp.local>
2024-03-14 18:17:36 -0700

    fixing dbt credentials

--- dbt/exampledbt/models/adventureworks/territory_details.sql
+++ dbt/exampledbt/models/adventureworks/territory_details.sql
@@ -3,7 +3,7 @@
     ) }}
 
 select  st.name as territory_name, date_part('year', soh.modifieddate) as year, date_part('month', soh.modifieddate) as month, sum(soh.totaldue) as total_due_per_territory_per_month
-from {{ ref('salesorderheader_incremental')}} as soh
+from {{ ref('salesorderheader')}} as soh
 left join sales.salesterritory st on soh.territoryid = st.territoryid
 group by st.name, date_part('year', soh.modifieddate), date_part('month', soh.modifieddate)
 order by year desc, total_due_per_territory_per_month
